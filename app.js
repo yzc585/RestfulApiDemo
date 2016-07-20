5e69ae400a67203906171c93f9639064c593a743
yuzhichao007@126.com <yuzhichao>
2016-07-20 17:31:28 +0800

    demo

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var wapApi = require("./restfulAPI.js");
+var app  = express();
+
+function WAP(){
+    var self = this;
+    self.connectMysql();
+};
+
+WAP.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : 'localhost',
+        user     : 'root',
+        password : 'yuzhichao',
+        database : 'restful_api_demo',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
+
+WAP.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new wapApi(router,connection,md5);
+      self.startServer();
+}
+
+WAP.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
+
+WAP.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new WAP();
---
+++ restfulAPI.js
@@ -0,0 +1,80 @@
+// 参考链接： http://blog.csdn.net/zxsrendong/article/details/17006185
+var mysql = require("mysql");
+function WAP_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+WAP_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    })
+    router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";        
+        var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+        query = mysql.format(query,table);
+        console.log(query);
+        connection.query(query,function(err,rows){
+            if(err) {
+            	console.log(err)
+                res.json({"Error" : true, "Message" : "Error executing MySQL query:" + query});
+            } else {
+                res.json({"Code" : 200, "Message" : "User Added !"});
+            }
+        });
+    });
+    router.get("/users",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["user_login"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+            	console.log(err)
+                res.json({"Error" : true, "Message" : "Error executing MySQL query:" + query});
+            } else {
+                res.json({"Code" : 200, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+    router.get("/users/:user_id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["user_login","user_id",req.params.user_id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+            	console.log(err)
+                res.json({"Error" : true, "Message" : "Error executing MySQL query:" + query});
+            } else {
+                res.json({"Code" : 200, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+    router.put("/users",function(req,res){
+        var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        var table = ["user_login","user_password",md5(req.body.password),"user_email",req.body.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+            	console.log(err)
+                res.json({"Error" : true, "Message" : "Error executing MySQL query:" + query});
+            } else {
+                res.json({"Code" : 200, "Message" : "Updated the password for email "+req.body.email});
+            }
+        });
+    });
+    router.delete("/users/:email",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["user_login","user_email",req.params.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+            	console.log(err)
+                res.json({"Error" : true, "Message" : "Error executing MySQL query:" + query});
+            } else {
+                res.json({"Code" : 200, "Message" : "Deleted the user with email "+req.params.email});
+            }
+        });
+    });
+}
+
+module.exports = WAP_ROUTER;
